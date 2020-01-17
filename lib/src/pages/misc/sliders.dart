5c90ad12cb0e8733bda9ed16826baea5e14bf5f3
张宇 <zylike@Hotmail.com>
2020-01-17 13:37:13 +0800

    1

--- lib/src/pages/misc/sliders.dart
+++ lib/src/pages/misc/sliders.dart
@@ -2,13 +2,13 @@
  * Author: Damodar Lohani
  * profile: https://github.com/lohanidamodar
   */
-  
+
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 class SlidersPage extends StatelessWidget {
   static final String path = "lib/src/pages/misc/sliders.dart";
-  final List<String> images= [
+  final List<String> images = [
     'assets/img/1.jpg',
     'assets/food/breakfast.jpg',
     'assets/travel/fishtail.jpg',
@@ -23,7 +23,7 @@ class SlidersPage extends StatelessWidget {
     'assets/travel/pashupatinath.jpg',
   ];
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sliders'),
@@ -32,31 +32,86 @@ class SlidersPage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Container(
+              width: 400,
               height: 300,
               color: Colors.grey.shade800,
               padding: EdgeInsets.all(16.0),
               child: Swiper(
+                scrollDirection: Axis.vertical,
                 itemBuilder: (BuildContext context, int index) {
-                  return ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image.asset(
-                      images[index],
-                      fit: BoxFit.cover,
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+
+                    ),
+                    height: 200,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Image.asset(
+                            images[index],
+//                            fit: BoxFit.fitWidth,
+//                            height: 200,
+                          ),
+                          Positioned(
+                            top: 0,
+                              left: 0,
+                              child: Container(
+                            child: InkWell(
+                              child: Text("aaa"),
+                              onTap: (){
+                                print("ninininini");
+                              },
+                            ),
+
+                            width: 50,
+                            height: 50,
+                            color: Colors.red,
+                          ))
+                        ],
+                      ),
                     ),
                   );
                 },
+                itemWidth: 200,
+
+                itemHeight: 200,
                 itemCount: 10,
-                viewportFraction: 0.8,
-                scale: 0.9,
-                pagination: SwiperPagination(),
+                layout: SwiperLayout.STACK,
               ),
             ),
+
+//            Container(
+//
+//              height: 300,
+//              color: Colors.grey.shade800,
+//              padding: EdgeInsets.all(16.0),
+//              child: Swiper(
+//                scrollDirection: Axis.vertical,
+//                itemBuilder: (BuildContext context, int index) {
+//                  return ClipRRect(
+//                    borderRadius: BorderRadius.circular(10.0),
+//                    child: Image.asset(
+//                      images[index],
+//                      fit: BoxFit.cover,
+//                    ),
+//                  );
+//                },
+//                itemCount: 10,
+//                viewportFraction: 0.8,
+//                scale: 0.9,
+//                pagination: SwiperPagination(),
+//              ),
+//            ),
             SizedBox(height: 20.0),
             Container(
               height: 300,
               color: Colors.grey.shade800,
               padding: EdgeInsets.all(16.0),
               child: Swiper(
+                scrollDirection: Axis.vertical,
                 itemBuilder: (BuildContext context, int index) {
                   return ClipRRect(
                     borderRadius: BorderRadius.circular(10.0),
@@ -66,48 +121,51 @@ class SlidersPage extends StatelessWidget {
                     ),
                   );
                 },
-                itemWidth: 300,
+                itemHeight: 200,
+                itemWidth: 200,
                 itemCount: 10,
                 layout: SwiperLayout.STACK,
               ),
             ),
             SizedBox(height: 20.0),
-            Container(
-              height: 340,
-              color: Colors.grey.shade800,
-              padding: EdgeInsets.all(16.0),
-              child: Swiper(
-                fade: 0.0,
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                    children: <Widget>[
-                      Container(
-                        height: 200,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0)),
-                          image: DecorationImage(
-                            image: AssetImage(images[index]),
-                            fit: BoxFit.cover
-                          )
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0))
-                        ),
-                        child: ListTile(
-                          subtitle: Text("awesome image caption"),
-                          title: Text("Awesome image"),))
-                    ],
-                  );
-                },
-                itemCount: 10,
-                scale: 0.9,
-                pagination: SwiperPagination(),
-              ),
-            ),
+//            Container(
+//              height: 340,
+//              color: Colors.grey.shade800,
+//              padding: EdgeInsets.all(16.0),
+//              child: Swiper(
+//                scrollDirection: Axis.vertical,
+//
+//                fade: 0.0,
+//                itemBuilder: (BuildContext context, int index) {
+//                  return Column(
+//                    children: <Widget>[
+//                      Container(
+//                        height: 200,
+//                        width: double.infinity,
+//                        decoration: BoxDecoration(
+//                          borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0)),
+//                          image: DecorationImage(
+//                            image: AssetImage(images[index]),
+//                            fit: BoxFit.cover
+//                          )
+//                        ),
+//                      ),
+//                      Container(
+//                        decoration: BoxDecoration(
+//                          color: Colors.white,
+//                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0))
+//                        ),
+//                        child: ListTile(
+//                          subtitle: Text("awesome image caption"),
+//                          title: Text("Awesome image"),))
+//                    ],
+//                  );
+//                },
+//                itemCount: 10,
+//                scale: 0.9,
+//                pagination: SwiperPagination(),
+//              ),
+//            ),
           ],
         ),
       ),
